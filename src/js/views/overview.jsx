d9544fbbb9a6137b449b815b0c6ea4ad40c0bc3c
Melanie Young <ms.melanieyoung@gmail.com>
2019-03-28 19:00:44 +0000

    created how it works cards with linked buttons

--- src/js/views/overview.jsx
+++ src/js/views/overview.jsx
@@ -17,8 +17,42 @@ export class Overview extends React.Component {
 								Some quick example text to build on the card
 								title and make up the bulk of the cardntent.
 							</p>
-							<a href="#" className="btn btn-primary">
-								Go somewhere
+							<a href="/sign_up" className="btn btn-primary">
+								Sign Up
+							</a>
+						</div>
+					</div>
+					<div className="col-3 card-text ml-5">
+						<img
+							className="card-img-top"
+							src="http://placehold.it/150x150"
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the cardntent.
+							</p>
+							<a href="/match" className="btn btn-primary">
+								Choose your game
+							</a>
+						</div>
+					</div>
+					<div className="col-3 card-text ml-5">
+						<img
+							className="card-img-top"
+							src="http://placehold.it/150x150"
+							alt="Card image cap"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the cardntent.
+							</p>
+							<a href="/profile" className="btn btn-primary">
+								Cash out
 							</a>
 						</div>
 					</div>
