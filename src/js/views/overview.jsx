1582b76835944dcdcfe23fcdea2ddd4ec0a2b64b
Melanie Young <ms.melanieyoung@gmail.com>
2019-03-29 23:11:34 +0000

    overview steps

--- src/js/views/overview.jsx
+++ src/js/views/overview.jsx
@@ -7,52 +7,56 @@ export class Overview extends React.Component {
 	render() {
 		return (
 			<div className="main">
-				<div className="row">
-					<div className="col-3 card-text ml-5">
+				<div className="row mx-5">
+					<div className="col-3 card-text ml-5 mt-5">
 						<img
-							className="card-img-top"
+							className="typing"
 							src={Typing}
 							alt="Card image cap"
 						/>
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
+							<h5 className="card-title">Step One</h5>
 							<p className="card-text">
-								Some quick example text to build on the card
-								title and make up the bulk of the cardntent.
+								<li>Sign up</li>
+								<li>Create profile</li>
+								<li>Link your cryptocurrency wallet</li>
 							</p>
 							<a href="/sign_up" className="btn btn-primary">
 								Sign Up
 							</a>
 						</div>
 					</div>
-					<div className="col-3 card-text ml-5">
+					<div className="col-3 mx-5">
 						<img
-							className="card-img-top"
+							className="controller"
 							src={Controller}
 							alt="Card image cap"
 						/>
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
+							<h5 className="card-title">Step Two</h5>
 							<p className="card-text">
-								Some quick example text to build on the card
-								title and make up the bulk of the cardntent.
+								<li>Choose your console</li>
+								<li>Choose your game</li>
+								<li>Place your bet</li>
+								<li>Find or wait for an opponent</li>
+								<li>Play</li>
 							</p>
 							<a href="/match" className="btn btn-primary">
 								Choose your game
 							</a>
 						</div>
 					</div>
-					<div className="col-3 card-text ml-5">
+					<div className="col-3">
 						<img
 							className="card-img-top"
 							src={Cashout}
 							alt="Card image cap"
 						/>
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
+							<h5 className="card-title">Step Three</h5>
 							<p className="card-text">
-								Some quick example text to build on the card
-								title and make up the bulk of the cardntent.
+								<li>Go to your profile</li>
+								<li>Cash out your winnings</li>
 							</p>
 							<a href="/profile" className="btn btn-primary">
 								Cash out
